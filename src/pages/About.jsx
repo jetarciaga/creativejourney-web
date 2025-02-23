1e574da1ead73b7b79c1bb3543b12fb60227a4d5
Jethro Arciaga <jet.arciaga@gmail.com>
2025-02-23 21:13:03 +0800

    Update to about page to be semantically correct

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,16 @@
 import "./About.scss";
 import teamImage from "../assets/images/team.jpg";
 import underwater from "../assets/images/underwater.jpg";
+import tarsier from "../assets/images/tarsier.jpg";
 
 const About = () => {
   return (
-    <section className="page-container">
-      <section className="text-wrapper">
-        <article>
-          <h1>Your Gateway to the Best Worry-Free Travel Deals!</h1>
+    <section className="about-container">
+      <div className="about-wrapper">
+        <article className="about-us">
+          <header>
+            <h1>Your Gateway to the Best Worry-Free Travel Deals!</h1>
+          </header>
           <p>
             Creative Journeys Travel PH is a premier wholesaler travel agency
             based in the Philippines, boasting 8 years of expertise in crafting
@@ -18,12 +21,12 @@ const About = () => {
             clients.
           </p>
         </article>
-        <aside>
-          <div className="image-container">
-            <img src={teamImage} alt="CreativeJourneys Team" />
-          </div>
-        </aside>
-      </section>
+
+        <figure className="image-container">
+          <img src={teamImage} alt="CreativeJourneys Team" />
+          <figcaption>Meet the CreativeJourneys PH Team</figcaption>
+        </figure>
+      </div>
       <p>
         Our extensive experience and deep understanding of the travel industry
         allow us to offer unparalleled service and insights, ensuring every
@@ -31,22 +34,41 @@ const About = () => {
         to detail, personalized service, and the ability to handle complex
         travel arrangements with ease.
       </p>
-      <section className="mission">
-        <section className="text-section">
-          <h2>Our Mission</h2>
-          <p>
-            At Creative Journeys PH, our mission is to transform travel dreams
-            into reality by providing exceptional, personalized travel
-            solutions. We are dedicated to delivering memorable experiences
-            through our expertise, commitment to excellence, and passion for
-            travel.
-          </p>
-        </section>
-        <section className="img-section">
-          <div className="mission-img">
-            <img src={underwater} alt="" />
+      <section className="company-profile">
+        <article className="mission">
+          <div className="text-content">
+            <header>
+              <h2>Our Mission</h2>
+            </header>
+            <p>
+              At Creative Journeys PH, our mission is to transform travel dreams
+              into reality by providing exceptional, personalized travel
+              solutions. We are dedicated to delivering memorable experiences
+              through our expertise, commitment to excellence, and passion for
+              travel.
+            </p>
           </div>
-        </section>
+          <figure className="img-crop">
+            <img src={underwater} alt="" />
+            <figcaption>Underwater adventure with turtle</figcaption>
+          </figure>
+        </article>
+        <article className="vision">
+          <div className="text-content">
+            <header>
+              <h2>Our Vision</h2>
+            </header>
+            <p>
+              To be one of the leading travel agencies in the Philippines, known
+              for our innovative solutions, exceptional service, and unwavering
+              dedication to exceeding client expectations.
+            </p>
+          </div>
+          <figure className="img-crop">
+            <img src={tarsier} alt="" />
+            <figcaption>Tarsier from Philippines</figcaption>
+          </figure>
+        </article>
       </section>
     </section>
   );
