e6e4298e8bfff5e647bd640c2294e7bb8aa43718
Jethro Arciaga <jet.arciaga@gmail.com>
2025-02-24 20:49:52 +0800

    Added underconstruction page

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Soon from "./pages/Soon";
 
 import "./App.scss";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <Router>
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Soon />} />
         </Routes>
       </MainLayout>
     </Router>
---
+++ src/pages/Soon.jsx
@@ -0,0 +1,16 @@
+import "./Soon.scss";
+
+const Soon = () => {
+  return (
+    <div className="soon-container">
+      <section>
+        <header>
+          <i className="bx bxs-error" />
+          <h1>This page is currently unavailable</h1>
+        </header>
+      </section>
+    </div>
+  );
+};
+
+export default Soon;
